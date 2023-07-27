caa64df9c494629d36510d8de87ebc8980b87483
Salimatah Agbadou <salimatahagbadou@gmail.com>
2023-07-27 23:43:52 +0100

    Updating website for portfolio

--- dist/App.d36a57b6.js
+++ dist/App.d36a57b6.js
@@ -32922,7 +32922,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54240" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54725" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
